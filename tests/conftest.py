9ccca5162c03601ae158462c037d76418e96e9cf
bb472 <bb472@njit.edu>
2024-12-18 14:36:31 +0000

    confest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,64 +1,53 @@
-# test_users.py
+from builtins import bool, int, str
+from pathlib import Path
+from pydantic import  Field, AnyUrl, DirectoryPath
+from pydantic_settings import BaseSettings
+from typing import ClassVar
 
-from builtins import len
-import pytest
-from httpx import AsyncClient
-from sqlalchemy.future import select
+class Settings(BaseSettings):
+    max_login_attempts: int = Field(default=3, description="Background color of QR codes")
+    # Server configuration
+    server_base_url: AnyUrl = Field(default='http://localhost', description="Base URL of the server")
+    server_download_folder: str = Field(default='downloads', description="Folder for storing downloaded files")
+    min_password_length: ClassVar[int] = 5
 
-from app.models.user_model import User, UserRole
-from app.utils.security import verify_password
+    # Security and authentication configuration
+    secret_key: str = Field(default="secret-key", description="Secret key for encryption")
+    algorithm: str = Field(default="HS256", description="Algorithm used for encryption")
+    access_token_expire_minutes: int = Field(default=30, description="Expiration time for access tokens in minutes")
+    admin_user: str = Field(default='admin', description="Default admin username")
+    admin_password: str = Field(default='secret', description="Default admin password")
+    debug: bool = Field(default=False, description="Debug mode outputs errors and sqlalchemy queries")
+    jwt_secret_key: str = Field(default="a_very_secret_key")
+    jwt_algorithm: str = Field(default="HS256")
+    access_token_expire_minutes: int = Field(default=15)
+    refresh_token_expire_minutes: int = Field(default=1440)
+    # Database configuration
+    database_url: str = Field(default='postgresql+asyncpg://user:password@postgres/myappdb', description="URL for connecting to the database")
 
-@pytest.mark.asyncio
-async def test_user_creation(db_session, verified_user):
-    """Test that a user is correctly created and stored in the database."""
-    result = await db_session.execute(select(User).filter_by(email=verified_user.email))
-    stored_user = result.scalars().first()
-    assert stored_user is not None
-    assert stored_user.email == verified_user.email
-    assert verify_password("MySuperPassword$1234", stored_user.hashed_password)
+    # Optional: If preferring to construct the SQLAlchemy database URL from components
+    postgres_user: str = Field(default='user', description="PostgreSQL username")
+    postgres_password: str = Field(default='password', description="PostgreSQL password")
+    postgres_server: str = Field(default='localhost', description="PostgreSQL server address")
+    postgres_port: str = Field(default='5432', description="PostgreSQL port")
+    postgres_db: str = Field(default='myappdb', description="PostgreSQL database name")
+    # Discord configuration
+    discord_bot_token: str = Field(default='NONE', description="Discord bot token")
+    discord_channel_id: int = Field(default=1234567890, description="Default Discord channel ID for the bot to interact", example=1234567890)
+    #Open AI Key 
+    openai_api_key: str = Field(default='NONE', description="Open AI Api Key")
+    send_real_mail: bool = Field(default=False, description="use mock")
+    # Email settings for Mailtrap
+    smtp_server: str = Field(default='smtp.mailtrap.io', description="SMTP server for sending emails")
+    smtp_port: int = Field(default=2525, description="SMTP port for sending emails")
+    smtp_username: str = Field(default='your-mailtrap-username', description="Username for SMTP server")
+    smtp_password: str = Field(default='your-mailtrap-password', description="Password for SMTP server")
 
-# Apply similar corrections to other test functions
-@pytest.mark.asyncio
-async def test_locked_user(db_session, locked_user):
-    result = await db_session.execute(select(User).filter_by(email=locked_user.email))
-    stored_user = result.scalars().first()
-    assert stored_user.is_locked
 
-@pytest.mark.asyncio
-async def test_verified_user(db_session, verified_user):
-    result = await db_session.execute(select(User).filter_by(email=verified_user.email))
-    stored_user = result.scalars().first()
-    assert stored_user.email_verified
+    class Config:
+        # If your .env file is not in the root directory, adjust the path accordingly.
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
 
-@pytest.mark.asyncio
-async def test_user_role(db_session, admin_user):
-    result = await db_session.execute(select(User).filter_by(email=admin_user.email))
-    stored_user = result.scalars().first()
-    assert stored_user.role == UserRole.ADMIN
-
-@pytest.mark.asyncio
-async def test_bulk_user_creation_performance(db_session, users_with_same_role_50_users):
-    result = await db_session.execute(select(User).filter_by(role=UserRole.AUTHENTICATED))
-    users = result.scalars().all()
-    assert len(users) == 50
-
-@pytest.mark.asyncio
-async def test_password_hashing(user):
-    assert verify_password("MySuperPassword$1234", user.hashed_password)
-
-@pytest.mark.asyncio
-async def test_user_unlock(db_session, locked_user):
-    locked_user.unlock_account()
-    await db_session.commit()
-    result = await db_session.execute(select(User).filter_by(email=locked_user.email))
-    updated_user = result.scalars().first()
-    assert not updated_user.is_locked
-
-@pytest.mark.asyncio
-async def test_update_professional_status(db_session, verified_user):
-    verified_user.update_professional_status(True)
-    await db_session.commit()
-    result = await db_session.execute(select(User).filter_by(email=verified_user.email))
-    updated_user = result.scalars().first()
-    assert updated_user.is_professional
-    assert updated_user.professional_status_updated_at is not None
+# Instantiate settings to be imported in your application
+settings = Settings()
